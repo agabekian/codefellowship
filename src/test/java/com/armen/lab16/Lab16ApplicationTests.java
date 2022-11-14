6c32b701c4a32b10bf0332897a1c1c415840a227
Armasconi <armasconi@Armasconis-MacBook-Pro.local>
2022-11-14 14:19:25 -0800

    init

---
+++ src/test/java/com/armen/lab16/Lab16ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.armen.lab16;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab16ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
